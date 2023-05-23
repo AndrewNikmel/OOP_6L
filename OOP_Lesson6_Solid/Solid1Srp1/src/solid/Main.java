025fa6f9ba9a1df93d8bf3a93282651e9f0e5e79
AndrewNikmel <melnikandrej1@gmail.com>
2023-05-23 18:56:25 +0300

    created class Drawing

--- OOP_Lesson6_Solid/Solid1Srp1/src/solid/Main.java
+++ OOP_Lesson6_Solid/Solid1Srp1/src/solid/Main.java
@@ -1,12 +1,14 @@
 package solid;
 
+import solid.srp.Drawing;
 import solid.srp.Point;
 import solid.srp.Square;
 
 public class Main {
     public static void main(String[] args) {
         Square square = new Square(new Point(1,1), 5);
+        Drawing drawing = new Drawing(5*2);
         System.out.printf("Площадь фигуры: %d \n", square.getArea());
-        square.draw();
+        drawing.draw();
     }
 }
