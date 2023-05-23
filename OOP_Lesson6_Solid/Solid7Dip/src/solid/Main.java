09421b69cf0d2b2b2797c42768b31237efb0460c
AndrewNikmel <melnikandrej1@gmail.com>
2023-05-23 22:24:52 +0300

    did the stuff with the reporting

--- OOP_Lesson6_Solid/Solid7Dip/src/solid/Main.java
+++ OOP_Lesson6_Solid/Solid7Dip/src/solid/Main.java
@@ -1,11 +1,13 @@
 package solid;
 
+import solid.dip.DisplayReport;
+import solid.dip.PrintReport;
 import solid.dip.Report;
 
 public class Main {
     public static void main(String[] args) {
         Report report = new Report();
         report.calculate();
-        report.output();
+        report.output(new DisplayReport());
     }
 }
